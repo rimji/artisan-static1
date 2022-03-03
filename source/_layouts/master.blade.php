af6d2b3ab9fc2deae87b48a4f8df5217eaa2257e
rimji <88181976+rimji@users.noreply.github.com>
2022-03-03 17:30:24 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav> 
-              <a href="https://www.miomio.in/"><img src="https://user-images.githubusercontent.com/88181976/156557700-1a7133d3-ccb8-43b7-bf9d-c6e28e6bc1f8.png" alt="MIOMIO" width="45" height="45"></a>
+              <a href="https://www.miomio.in/"><img src="https://user-images.githubusercontent.com/88181976/155539273-650e05e9-765a-49ca-aa19-2788e14fc856.png" alt="MIOMIO" width="45" height="45"></a>
             </nav>
         </header>
 
