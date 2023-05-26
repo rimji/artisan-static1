47adbbaec2483e76d1d6d52a16762609be13cfc1
rimji <88181976+rimji@users.noreply.github.com>
2023-05-26 22:08:35 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,14 +3,5 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
-
-    <p>My name is {{ $page->owner->name }}</p>
-
-    <h2>Links:</h2>
-
-    <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
-    </ul>
+    
 @endsection
