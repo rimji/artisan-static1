553f2020e9c0f8cd761113cc72979141d81752a2
rimji <88181976+rimji@users.noreply.github.com>
2023-05-26 22:27:44 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'About')
 
 @section('content')
-    
+    <a href="https://www.facebook.com/rimsimofficial">Facebook</a>
+
+             &nbsp&nbsp
+
+             <a href="https://www.instagram.com/rimsimofficial">Instagram</a>
+
+             &nbsp&nbsp
+
+             <a href="https://www.twitter.com/rimsimofficial">Twitter</a>
+
+             &nbsp&nbsp
 @endsection
