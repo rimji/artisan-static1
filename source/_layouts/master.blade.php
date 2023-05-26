b1cc9cab4942e89d14761c624d20366e65103bc7
rimji <88181976+rimji@users.noreply.github.com>
2023-05-26 22:05:37 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -37,15 +37,7 @@
         
         <footer>
          <small>
-             <a href="https://www.facebook.com/rimsimofficial">Facebook</a>
-             &nbsp&nbsp
-             <a href="https://www.instagram.com/rimsimofficial">Instagram</a>
-             &nbsp&nbsp
-             <a href="https://www.twitter.com/rimsimofficial">Twitter</a>
-
-             &nbsp&nbsp
-             <a href="https://www.rimsim.com/contact/">Contact us</a>
-             <br>
+             
              &copy; 2023 RimSim
          </small>
         </footer>
