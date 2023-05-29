dcfcbd38e00ea4b540377a0bb4444fceae7ca32d
rimji <88181976+rimji@users.noreply.github.com>
2023-05-29 19:57:42 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -51,7 +51,7 @@
 
              <a href="https://www.rimsim.com/contact/">Contact us</a>
              <br>
-             &copy; 2023 RIMSIM
+             &copy; 2023 Rimsim
          </small>
         </footer>
     </section>
