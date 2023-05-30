cec73ccb607455037ccf14e7ec26115bf602d496
rimji <88181976+rimji@users.noreply.github.com>
2023-05-30 10:40:43 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,8 +4,8 @@
 
   <small>
 
-       How can we help you today?      
-
+       <img src="https://user-images.githubusercontent.com/88181976/241853506-b25887aa-504d-45c3-9506-10dbc733c1b3.png" alt="RIMSIM" width="250" height="250">
+            
 </small>
 
 
