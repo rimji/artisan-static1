39a1be7b87ba35f4a0957e67ba4ac3b47ba17065
rimji <88181976+rimji@users.noreply.github.com>
2023-05-31 13:58:50 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'production' => false,
     'baseUrl' => 'https://rimsim.com',
     'site' => [
-        'title' => 'Rimsim',
+        'title' => 'RIMSIM',
         'description' => 'How can we help you today?',
         'image' => 'default-share.png',
     ],
