5113affd9f3b5aa0c273305d06ba5484beb48a59
rimji <88181976+rimji@users.noreply.github.com>
2023-06-05 14:59:10 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
      <header>
             <nav>
-              <a href="https://www.rimsim.com/"><img src="https://user-images.githubusercontent.com/88181976/242913555-3f4b3bd7-e9cb-423b-83f7-90193bd39df6.png" alt="Rimsim" width="95" height="25"></a>
+              <a href="https://www.rimsim.com/"><img src="https://user-images.githubusercontent.com/88181976/243312295-d71385b7-3535-4d06-828b-74eb3ea9919f.png" alt="Rimsim" width="95" height="25"></a>
             </nav>
         </header> 
         
