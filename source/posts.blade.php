d031cedbcf92aba19e4be0d51ed9be528f5cc546
rimji <88181976+rimji@users.noreply.github.com>
2023-06-06 00:06:11 +0530

    Update posts.blade.php

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -6,7 +6,7 @@
     
 
     <ul>
-        @forelse ($posts->sortBy('date') as $post)
+        @forelse ($posts->sortBy('title') as $post)
             <li>
                 <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
                 <small>{{ $post->prettyDate() }}</small>
