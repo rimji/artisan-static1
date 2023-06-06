2c730e0c84316be6162c4e15d3158b105d8f532d
rimji <88181976+rimji@users.noreply.github.com>
2023-06-06 15:00:37 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -52,7 +52,7 @@
          
         </footer>
     </section>
-
+    <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     @includeWhen($page->production, '_partials.analytics')
     @include('_partials.cms.identity_redirect')
 </body>
