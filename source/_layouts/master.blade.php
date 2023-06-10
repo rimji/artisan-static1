2068d6e8c6f38eea2292557cc8bec297fb6ff82e
rimji <88181976+rimji@users.noreply.github.com>
2023-06-10 09:23:24 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -21,6 +21,7 @@
      <header>
             <nav>
               <a href="https://www.rimsim.com/"><img src="https://user-images.githubusercontent.com/88181976/244339648-4af88aa9-7877-47af-9cc0-3c5ecee391be.png" alt="Rimsim" width="90" height="25"></a>
+           <br> Save Money! Get Best Deals ⭐
             </nav>
         </header> 
         
