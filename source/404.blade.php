644a27e2563649f41759646c566d4f628d206e82
rimji <88181976+rimji@users.noreply.github.com>
2023-06-10 19:41:59 +0530

    Update 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -7,7 +7,7 @@ permalink: 404.html
 @section('title', 'Page not found')
 
 @section('content')
-    <h1>404 Error</h1>
-
-    <p>The page you were trying to access could not be found.</p>
+   404 Error
+    <br>
+    The page you were trying to access could not be found.
 @endsection
