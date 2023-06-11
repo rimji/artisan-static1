a9e3059687940bddab5741221d6af8e572e74efe
rimji <88181976+rimji@users.noreply.github.com>
2023-06-11 11:17:00 +0530

    Update deals-2.blade.php

--- source/deals-2.blade.php
+++ source/deals-2.blade.php
@@ -9,6 +9,7 @@
 <img src="https://user-images.githubusercontent.com/88181976/244913225-e359d639-8aaf-42b7-b7d8-98914934f356.png" alt="ghi">
 <img src="https://user-images.githubusercontent.com/88181976/244913461-52f1c949-9e7c-405d-b7b5-939c02b44699.png" alt="jkl">
 <img src="https://user-images.githubusercontent.com/88181976/244915263-467aa0d1-724a-49bb-ac1b-da1432f5bdff.png" alt="mno">
+<a href="https://www.rimsim.com/contact">Previous Deals</a>&nbsp&nbsp
 <a href="https://www.rimsim.com/contact">Next Deals</a>
       </small>
   
