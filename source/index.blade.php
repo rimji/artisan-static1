1e2e3dfcbd865c03d7e6b41b5fb83f934e548668
rimji <88181976+rimji@users.noreply.github.com>
2023-06-11 10:18:16 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,6 +7,7 @@
 <img src="https://user-images.githubusercontent.com/88181976/244913220-0e584bf0-0dec-4fd4-9ab8-622636bcd17f.png" alt="abc">
 <img src="https://user-images.githubusercontent.com/88181976/244913223-a974cdfd-f15b-490c-87a5-660def70aa11.png" alt="def">
 <img src="https://user-images.githubusercontent.com/88181976/244913225-e359d639-8aaf-42b7-b7d8-98914934f356.png" alt="ghi">
+<img src="https://user-images.githubusercontent.com/88181976/244913461-52f1c949-9e7c-405d-b7b5-939c02b44699.png" alt="jkl">
        </small>
   
   
