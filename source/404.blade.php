10d9e37b26ca7f82dce073365fdf7abde53e859e
rimji <88181976+rimji@users.noreply.github.com>
2023-06-11 10:38:20 +0530

    Update 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -7,8 +7,7 @@ permalink: 404.html
 @section('title', 'Page not found')
 
 @section('content')
-   <small>404 Error
-    <br>
-    The page you were trying to access could not be found.
-    </small>
+   <small>
+  <img src="https://user-images.githubusercontent.com/88181976/244914465-86ed7eb4-c718-4751-a785-28a29ce49d0e.png" alt="404"> 
+   </small>
 @endsection
