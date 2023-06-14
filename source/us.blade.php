e043f2c6ced2410ac67f8a4fd9fc7a3573ff1443
rimji <88181976+rimji@users.noreply.github.com>
2023-06-14 20:19:03 +0530

    Update us.blade.php

--- source/us.blade.php
+++ source/us.blade.php
@@ -5,6 +5,11 @@
 @section('content')
 
    <small>
-   contact@rimsim.com
+   <img src="{{ media('q_auto:good/ybb.png') }}" alt="gihi">
+  <img src="{{ media('q_auto:eco/ybb.png') }}" alt="gihi">
+  <img src="{{ media('ybb.png') }}" alt="gihi">
+<img src="{{ media('q_auto/ybb.jpg') }}" alt="gihi">
+
+
    </small>
 @endsection
