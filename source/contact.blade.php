4992e215154303b01082ef24f351b14752ae7fbe
rimji <88181976+rimji@users.noreply.github.com>
2023-06-14 20:18:00 +0530

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -6,12 +6,7 @@
 
    <small>
    <mark>in.contact@rimsim.com</mark>
-   <img src="{{ media('q_auto:good/ybb.png') }}" alt="gihi">
-  <img src="{{ media('q_auto:eco/ybb.png') }}" alt="gihi">
-  <img src="{{ media('ybb.png') }}" alt="gihi">
-<img src="{{ media('q_auto/ybb.jpg') }}" alt="gihi">
-
-
+   
 
 
  
