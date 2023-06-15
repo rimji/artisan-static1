bfd37f60e9fcff6b5dcdcfa6b8bf32c1f84a8ecd
rimji <88181976+rimji@users.noreply.github.com>
2023-06-15 09:30:13 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -5,15 +5,11 @@
 @section('content')
 
    <small>
-   <img src="{{ media('q_auto:good/ybb.png') }}" alt="gihi">
-  <img src="{{ media('q_auto:eco/ybb.png') }}" alt="gihi">
-  <img src="{{ media('ybb.png') }}" alt="gihi">
-<img src="{{ media('q_auto/ybb.jpg') }}" alt="gihi">
-<a href="https://www.rimsim.com/in-deals-2"><b>Prev Deals</b></a>
 
-
-
-
-
-   </small>
+<img src="https://user-images.githubusercontent.com/88181976/244913220-0e584bf0-0dec-4fd4-9ab8-622636bcd17f.png" alt="mno">
+  
+  <a href="https://www.rimsim.com/index"><b>Prev Deals</b></a>
+ 
+      </small>
+  
 @endsection
