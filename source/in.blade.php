510b854d95e94b15799698579bf70df741322b4e
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 18:32:41 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -9,9 +9,6 @@
 
 <img src="https://user-images.githubusercontent.com/88181976/244913223-a974cdfd-f15b-490c-87a5-660def70aa11.png" alt="def">
 
+<a href="https://www.rimsim.com/in-deals-2"><img src="https://user-images.githubusercontent.com/88181976/246421096-7591e416-28a5-4bbb-a145-fb8c8f2c058d.jpg" alt="Previous" width="25" height="25"></a>
 
-<a href="https://www.rimsim.com/in-deals-2">Prev Deals</a>
-
-
-   </small>
 @endsection
