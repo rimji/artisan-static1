7af283f52f1407f8bb22f27bd0ff2cd4df493f0a
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 18:41:17 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -5,6 +5,6 @@
 @section('content')
    <small>
 <img src="https://user-images.githubusercontent.com/88181976/244913223-a974cdfd-f15b-490c-87a5-660def70aa11.png" alt="def">
-<a href="https://www.rimsim.com/in-deals-2">Prev Deals</a>
+<a href="https://www.rimsim.com/in-deals-2">Next Deal</a>
    </small>
 @endsection
