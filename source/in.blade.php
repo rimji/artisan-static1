f91fb44c45b84ef72639ff9b865a7fd1fd00fbbe
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 22:32:32 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Shopping made easy!')
 
 @section('content')
-   <small>
+   
       <div class="scroll-container">
   <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="250" height="333">
   <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="250" height="333">
@@ -13,5 +13,5 @@
       </div>
    <a href="https://www.rimsim.com/in-deals-2"><mark><b>Buy it Now!</b></mark></a><br>
    <a href="https://www.rimsim.com/in-deals-2">See Next</a>
-    </small>
+    
 @endsection
