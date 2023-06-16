783c55014911c6352e2579116d0a5604137bb30a
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 18:34:46 +0530

    Update in-deals-2.blade.php

--- source/in-deals-2.blade.php
+++ source/in-deals-2.blade.php
@@ -5,11 +5,8 @@
 @section('content')
 
    <small>
-   
-
 <img src="https://user-images.githubusercontent.com/88181976/244913223-a974cdfd-f15b-490c-87a5-660def70aa11.png" alt="def">
-<a href="https://www.rimsim.com/"><img src="https://user-images.githubusercontent.com/88181976/246421087-1203cbd2-4cad-4c79-91c8-d68d8961f7f6.jpg" alt="Next" width="25" height="25"></a>&nbsp&nbsp&nbsp&nbsp
-&nbsp&nbsp&nbsp&nbsp
-<a href="https://www.rimsim.com/"><img src="https://user-images.githubusercontent.com/88181976/246421096-7591e416-28a5-4bbb-a145-fb8c8f2c058d.jpg" alt="Previous" width="25" height="25"></a>
-</small>
+<a href="https://www.rimsim.com/in">Next Deals</a>&nbsp&nbsp&nbsp&nbsp
+<a href="https://www.rimsim.com/in-deals-3">Prev Deals</a>
+   </small>
 @endsection
