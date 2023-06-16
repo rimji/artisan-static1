d027165ce7dccf1e59c72e90c07a83592d30db38
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 21:08:38 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -5,11 +5,11 @@
 @section('content')
    <small>
       <div class="scroll-container">
-  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="250" height="333">
-  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="250" height="333">
-  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="250" height="333">
-  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="250" height="333">
-  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="250" height="333">
+  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="290" height="386">
+  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="290" height="386">
+  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="290" height="386">
+  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="290" height="386">
+  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="290" height="386">
       </div>
 <a href="https://www.rimsim.com/in-deals-2">Next Deal</a>
    </small>
