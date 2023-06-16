9410e1cd5904fab7fd3d31f1441267486854d297
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 15:27:36 +0530

    Update in-deals-2.blade.php

--- source/in-deals-2.blade.php
+++ source/in-deals-2.blade.php
@@ -10,8 +10,8 @@
 <img src="https://user-images.githubusercontent.com/88181976/244913223-a974cdfd-f15b-490c-87a5-660def70aa11.png" alt="def">
 
 
-<a href="https://www.rimsim.com/in-deals-2">Next Deals</a>&nbsp&nbsp&nbsp&nbsp
-<a href="https://www.rimsim.com/in-deals-2">Next Deals</a>
+<a href="https://www.rimsim.com/in">Next Deals</a>&nbsp&nbsp&nbsp&nbsp
+<a href="https://www.rimsim.com/in-deals-3">Prev Deals</a>
 
    </small>
 @endsection
