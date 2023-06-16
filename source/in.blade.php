afb9d450366531da100f4f0a8b5d12c778c83d64
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 18:37:43 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -3,12 +3,8 @@
 @section('title', 'Shopping made easy!')
 
 @section('content')
-
    <small>
-   
-
 <img src="https://user-images.githubusercontent.com/88181976/244913223-a974cdfd-f15b-490c-87a5-660def70aa11.png" alt="def">
-
-<a href="https://www.rimsim.com/in-deals-2"><img src="https://user-images.githubusercontent.com/88181976/246421096-7591e416-28a5-4bbb-a145-fb8c8f2c058d.jpg" alt="Previous" width="25" height="25"></a>
-
+<a href="https://www.rimsim.com/in-deals-2">Prev Deals</a>
+   </small>
 @endsection
