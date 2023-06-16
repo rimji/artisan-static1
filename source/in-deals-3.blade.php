ad817ee157da3613bec037fee99af2d861537afb
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 18:46:42 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -8,7 +8,7 @@
 
 <img src="https://user-images.githubusercontent.com/88181976/244915263-467aa0d1-724a-49bb-ac1b-da1432f5bdff.png" alt="mno">
   
-  <a href="https://www.rimsim.com/about">Prev Deals</a>
+  <a href="https://www.rimsim.com/about">Next Deal</a>
  
       </small>
   
