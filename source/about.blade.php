19961c9f947860cb39ed8726d80720ebb0f8ac8e
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 18:52:40 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -8,7 +8,7 @@
   <img src="{{ media('q_auto:eco/ybb.png') }}" alt="gihi">
   <img src="{{ media('ybb.png') }}" alt="gihi">
 <img src="{{ media('q_auto/ybb.jpg') }}" alt="gihi">
-<a href="https://www.rimsim.com/in-deals-2"><b>Prev Deals</b></a>
+<a href="https://www.rimsim.com/in-deals-2">Prev Deal</a>
 
 
 
