30fdf505933092aa905cfe823c4374c8382ff314
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 20:31:52 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -5,8 +5,8 @@
 @section('content')
    <small>
       <div class="scroll-container">
-  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre">
-  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest">
+  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="420" height="559">
+  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="420" height="559">
   <img src="https://user-images.githubusercontent.com/88181976/244915263-467aa0d1-724a-49bb-ac1b-da1432f5bdff.png" alt="Northern Lights">
   <img src="{{ media('q_auto/ybb.jpg') }}" alt="Mountains">
       </div>
