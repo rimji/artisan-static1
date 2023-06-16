7698b332beabf1df8068cd189fadf02896e0836a
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 19:36:27 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -10,5 +10,6 @@
   <img src="https://user-images.githubusercontent.com/88181976/244915263-467aa0d1-724a-49bb-ac1b-da1432f5bdff.png" alt="Northern Lights">
   <img src="{{ media('q_auto/ybb.jpg') }}" alt="Mountains">
       </div>
-
+<a href="https://www.rimsim.com/in-deals-2">Next Deal</a>
+   </small>
 @endsection
