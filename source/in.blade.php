f88bbb888129c145829caea458b753cb1a9beff9
rimji <88181976+rimji@users.noreply.github.com>
2023-06-16 20:43:32 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -7,8 +7,9 @@
       <div class="scroll-container">
   <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="250" height="333">
   <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="250" height="333">
-  <img src="https://user-images.githubusercontent.com/88181976/244915263-467aa0d1-724a-49bb-ac1b-da1432f5bdff.png" alt="Northern Lights">
-  <img src="{{ media('q_auto/ybb.jpg') }}" alt="Mountains">
+  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="250" height="333">
+  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="250" height="333">
+  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="250" height="333">
       </div>
 <a href="https://www.rimsim.com/in-deals-2">Next Deal</a>
    </small>
