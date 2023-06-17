be7e4f492ccea1527157d152820f89ae4e413cc5
rimji <88181976+rimji@users.noreply.github.com>
2023-06-17 11:30:28 +0530

    Update 404.blade.php

--- source/404.blade.php
+++ source/404.blade.php
@@ -7,6 +7,6 @@ permalink: 404.html
 @section('title', 'Page not found')
 
 @section('content')
-     <small><b>Oops...</b><br>We can't find the page you are looking for.</small><br><br>
+     <small><b>Oops...</b><br>We can't find the page you are looking for.</small><br>
      <a href="https://www.rimsim.com/" class="button"><b>BACK TO HOME</b></a>
 @endsection
