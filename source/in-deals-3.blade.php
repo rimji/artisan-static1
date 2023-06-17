62de43e46fcc6710bd14788465e02c62f9e43cc6
rimji <88181976+rimji@users.noreply.github.com>
2023-06-17 18:39:58 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 
    <div class="scroll-container">
-  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="400" height="533">
-  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="400" height="533">
+  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="350" height="466">
+  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="350" height="466">
   <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="400" height="533">
   <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="400" height="533">
   <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="400" height="533">
