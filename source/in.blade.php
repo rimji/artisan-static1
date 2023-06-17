68ba8ebf93901d94488629204b618401a19ba8b8
rimji <88181976+rimji@users.noreply.github.com>
2023-06-17 22:23:35 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -10,5 +10,6 @@
   <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="270" height="360">
   <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="270" height="360">
       </div><br>
+ Kids Squeeze Toy<br>
   <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY it NOW</b></a>
 @endsection
