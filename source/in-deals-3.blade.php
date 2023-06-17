cc91ef53ee1bd16b52ccecad6d8f62d72b292c85
rimji <88181976+rimji@users.noreply.github.com>
2023-06-17 18:41:30 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -7,9 +7,9 @@
    <div class="scroll-container">
   <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="350" height="466">
   <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="350" height="466">
-  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="400" height="533">
-  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="400" height="533">
-  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="400" height="533">
+  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="350" height="466">
+  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="350" height="466">
+  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="350" height="466">
    </div><br>
   <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY it NOW</b></a>
 
