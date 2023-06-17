1333dc891deefdc1b2838ab4485f2bf82d9d31e8
rimji <88181976+rimji@users.noreply.github.com>
2023-06-17 20:32:11 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
    <div class="scroll-container">
-  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="325" height="433">
-  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="325" height="433">
-  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="325" height="433">
-  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="325" height="433">
-  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="325" height="433">
+  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="370" height="493">
+  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="370" height="493">
+  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="370" height="493">
+  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="370" height="493">
+  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="370" height="493">
    </div><br>
   <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY it NOW</b></a>
 
