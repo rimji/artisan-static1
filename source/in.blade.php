50bc2dd19d15ecdaf5ebbf23f20179ed129437fd
rimji <88181976+rimji@users.noreply.github.com>
2023-06-18 16:38:18 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -10,5 +10,5 @@
   <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="270" height="360">
   <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="270" height="360">
       </div><br>
-  <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY it NOW</b></a>
+  <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY NOW</b></a>
 @endsection
