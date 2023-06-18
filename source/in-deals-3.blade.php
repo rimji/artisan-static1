84a95bbd8be874b691ccbf3695a8090e6ec54a03
rimji <88181976+rimji@users.noreply.github.com>
2023-06-18 20:34:28 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -11,9 +11,6 @@
   <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="370" height="493">
   <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="370" height="493">
    </div><br>
-  <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY it NOW</b></a>
-<small>
-<br>
-<br><br>To create a fixed top menu<br><br>, use position:fixed and top:0<br><br>. Note that the fixed menu will overlay your other<br><br> content. To fix this, add a margin-top (to the content) that is equal or l<br><br>arger than the height of your menu.
-</small>
+  <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY NOW</b></a>
+
 @endsection
