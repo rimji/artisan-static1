2b46d82200eaf320ada041a325c234203254dd1d
rimji <88181976+rimji@users.noreply.github.com>
2023-06-18 19:48:25 +0530

    Update in-deals-3.blade.php

--- source/in-deals-3.blade.php
+++ source/in-deals-3.blade.php
@@ -13,5 +13,5 @@
    </div><br>
   <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY it NOW</b></a>
 <br>
-To create a fixed top menu, use position:fixed and top:0. Note that the fixed menu will overlay your other content. To fix this, add a margin-top (to the content) that is equal or larger than the height of your menu.
+<br><br>To create a fixed top menu<br><br>, use position:fixed and top:0<br><br>. Note that the fixed menu will overlay your other<br><br> content. To fix this, add a margin-top (to the content) that is equal or l<br><br>arger than the height of your menu.
 @endsection
