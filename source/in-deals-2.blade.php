bdf3dac32d700cb85e26dbbbd55d5cd03cb30cbd
rimji <88181976+rimji@users.noreply.github.com>
2023-06-18 15:06:01 +0530

    Update in-deals-2.blade.php

--- source/in-deals-2.blade.php
+++ source/in-deals-2.blade.php
@@ -7,6 +7,7 @@
    <small>
 <img src="https://user-images.githubusercontent.com/88181976/244913220-0e584bf0-0dec-4fd4-9ab8-622636bcd17f.png" alt="def">
 <a href="https://www.rimsim.com/in">Prev Deal</a>&nbsp&nbsp&nbsp&nbsp
+<br>
 <a href="https://www.rimsim.com/in-deals-3">Next Deal</a>
    </small>
 @endsection
