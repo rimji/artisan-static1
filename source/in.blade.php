20825683cd3185f80193872e91354f51b3c7f754
rimji <88181976+rimji@users.noreply.github.com>
2023-06-19 15:41:20 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -10,6 +10,11 @@
   <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="270" height="360">
   <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="270" height="360">
       </div><br>
+Various collaborative online encyclopedias were attempted before the start of Wikipedia, but with limited success.[17] Wikipedia began as a complementary project for Nupedia, a free online English-language encyclopedia project whose articles were written by experts and reviewed under a formal process.[18] It was founded on March 9, 2000, under the ownership of Bomis, a web portal company. Its main figures were Bomis CEO Jimmy Wales and Larry Sanger, editor-in-chief for Nupedia and later Wikipedia.[1][19] Nupedia was initially licensed under its own Nupedia Open Content License, but before Wikipedia was founded, Nupedia switched to the GNU Free Documentation License at the urging of Richard Stallman.[20] Wales is credited with defining the goal of making a publicly editable encyclopedia,[21][22] while Sanger is credited with the strategy of using a wiki to reach that goal.[23] On January 10, 2001, Sanger proposed on the Nupedia mailing list to create a wiki as a "feeder" project for Nupedia.[24]
+
+Launch and growth
+The domains wikipedia.com (later redirecting to wikipedia.org) and wikipedia.org were registered on January 12, 2001,[25] and January 13, 2001,[26] respectively, and Wikipedia was launched on January 15, 2001[18] as a single English-language edition at www.wikipedia.com,[27] and announced by Sanger on the Nupedia mailing list.[21] The name originated from a blend of the words wiki and encyclopedia.[28][29] Its integral policy of "neutral point-of-view"[30] was codified in its first few months. Otherwise, there were initially relatively few rules, and it operated independently of Nupedia.[21] Bomis originally intended it as a business for profit.[31]
+
   <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY NOW</b></a>
 
 <fooz>
