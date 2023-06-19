a74ef3dab2723e0e51f8a57e67544b61fd1babe4
rimji <88181976+rimji@users.noreply.github.com>
2023-06-19 13:35:47 +0530

    Update in.blade.php

--- source/in.blade.php
+++ source/in.blade.php
@@ -11,4 +11,7 @@
   <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="270" height="360">
       </div><br>
   <a href="https://www.rimsim.com/in-deals-2" class="button"><b>BUY NOW</b></a>
+Initially available only in English, versions in other languages were quickly developed. Wikipedia's combined editions comprise more than 61 million articles, attracting around 2 billion unique device visits per month and more than 15 million edits per month (about 5.7 edits per second on average) as of January 2023.[6][7]
+
+Wikipedia has been praised for its enablement of the democratization of knowledge, extent of coverage, unique structure, culture, and reduced degree of commercial bias. It has been criticized for exhibiting systemic bias, particularly gender bias against women and ideological bias.[8][9] While the reliability of Wikipedia was frequently criticized in the 2000s, it has improved over time, receiving greater praise in the late 2010s and early 2020s,[3][8][10][note 4] having become an important fact-checking site.[11][12] It has been censored by world governments, ranging from specific pages to the entire site.[13][14] Articles on breaking news are often accessed as a source of frequently updated information about those events.[15][16]
 @endsection
