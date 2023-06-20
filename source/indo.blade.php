491f4e0f623b708d08308727f2368f5fe05f1061
rimji <88181976+rimji@users.noreply.github.com>
2023-06-20 20:27:13 +0530

    Create indo.blade.php

---
+++ source/indo.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('title', 'Shopping made easy!')
+
+@section('content')
+      <div class="scroll-container">
+  <img src="{{ media('q_auto/abz1.jpg') }}" alt="Cinque Terre" width="270" height="360">
+  <img src="{{ media('q_auto/dez1.jpg') }}" alt="Forest" width="270" height="360">
+  <img src="{{ media('q_auto/jkz.jpg') }}" alt="Cinque Terre" width="270" height="360">
+  <img src="{{ media('q_auto/mnz.jpg') }}" alt="Forest" width="270" height="360">
+  <img src="{{ media('q_auto/ghz.jpg') }}" alt="Cinque Terre" width="270" height="360">
+      </div><br>
+  <a href="https://www.rimsim.com/in-deals-2" class="button2"><b>BUY NOW</b></a>
+@endsection
